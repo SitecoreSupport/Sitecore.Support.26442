b98b1b293b8f0f52f793d4817bfe0bd105a4b2ca
Kirill Volovitskiy <kiv@sitecore.net>
2017-11-22 11:30:28 +0200

    Default code

---
+++ src/Sitecore.Support.26442/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.26442")]
+[assembly: AssemblyProduct("Sitecore.Support.26442")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.26442/Publishing/Service/PublishingServiceConfigurator.cs
@@ -0,0 +1,7 @@
+﻿namespace Sitecore.Support.Publishing.Service
+{
+  using Sitecore.Publishing.Service;
+  public class PublishingServiceConfigurator : PublishConfigurator<PublishManager>
+  {
+  }
+}
